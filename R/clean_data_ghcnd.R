40f420b2390d040e9bd2b0c1f0b4824c05d7bbd2
alexander <aweyant@ucsd.edu>
2026-02-26 11:03:36 -0800

    Commit before consolidating some of the logic in clean_raw_ghcnd_to_standard

--- R/clean_data_ghcnd.R
+++ R/clean_data_ghcnd.R
@@ -44,7 +44,9 @@
 #' Flag ``L'' means that a temperature observation is probably offset from its
 #' purported calendar day. This is a dealbreaker for spatial interpolation.
 #'
-#' @returns a data.frame with a GHCNd record in a more sensible format
+#' @returns a data.frame with a GHCNd record in a more sensible format; NB, the
+#' default behaviour is such that no data are removed by this function.
+#'
 #' @export
 #'
 #' @examples
@@ -64,25 +66,39 @@
 #' }
 clean_raw_ghcnd_to_standard <- function(raw_ghcnd_df, select_SOD_cols = TRUE, begin_date = NULL, end_date = NULL) {
   raw_ghcnd_df %>%
+    # Add blank/NA rows for any days completely missing from the record
+    {
+      dplyr::left_join(
+        tibble::tibble(
+          DATE = seq.DATE(from = ifelse(is.null(begin_date), min((.)$DATE), begin_date),
+                          to = ifelse(is.null(end_date), max((.)$DATE), end_date),
+                          by = '1 day')
+        ),
+        (.),
+        by = "DATE"
+      )
+    } %>%
+    # Convert units of tmin, tmax, and precip
+    dplyr::mutate(
+      dplyr::across(
+        .cols = tidyselect::any_of(c("TMIN", "TMAX", "PRCP", "TAVE")),
+        .fns = \(x) x/10)
+      ) %>%
     {
       if(all(c("TMAX_ATTRIBUTES", "TMIN_ATTRIBUTES", "PRCP_ATTRIBUTES") %in%
              names(.))) {
         (. %>%
            dplyr::mutate(
              dplyr::across(
-               .cols = tidyselect::all_of(c("TMAX_ATTRIBUTES",
-                                            "TMIN_ATTRIBUTES",
-                                            "PRCP_ATTRIBUTES")),
+               .cols = tidyselect::all_of(
+                 c("TMAX_ATTRIBUTES","TMIN_ATTRIBUTES", "PRCP_ATTRIBUTES")
+                 ),
                .fns = \(x) {dplyr::if_else(is.na(x), ",,", x)}
-             )
-           ) %>%
-           tidyr::separate_wider_delim(cols = tidyselect::all_of(c("TMAX_ATTRIBUTES",
-                                                                   "TMIN_ATTRIBUTES",
-                                                                   "PRCP_ATTRIBUTES")),
-                                       delim = ",",
-                                       names_sep = "_",
-                                       too_few = "align_start",
-                                       too_many = "drop") %>%
+               )
+             ) %>%
+           tidyr::separate_wider_delim(
+             cols = tidyselect::all_of(c("TMAX_ATTRIBUTES","TMIN_ATTRIBUTES","PRCP_ATTRIBUTES")),
+             delim = ",", names_sep = "_", too_few = "align_start", too_many = "drop") %>%
            dplyr::mutate(TMAX = dplyr::case_when(TMAX_ATTRIBUTES_1 %in% c("L") ~ NA,
                                                  TMAX_ATTRIBUTES_2 != "" ~ NA,
                                                  TRUE ~ TMAX),
@@ -98,24 +114,12 @@ clean_raw_ghcnd_to_standard <- function(raw_ghcnd_df, select_SOD_cols = TRUE, be
       }
       else .
     } %>%
-    dplyr::rename_with(.cols = tidyselect::everything(),
-                       .fn = tolower) %>%
+    # Make all column names lower case
+    dplyr::rename_with(.cols = tidyselect::everything(), .fn = tolower) %>%
+    # Rename "prcp" to "precip"
     dplyr::rename_with(.cols = tidyselect::any_of("prcp"), .fn = \(str){return("precip")}) %>%
-    dplyr::mutate(dplyr::across(
-      .cols = tidyselect::any_of(c("tmin", "tmax", "precip", "tave")),
-      .fns = \(x) x/10)) %>%
-    # Adds blank/NA rows for any days completely missing from the record
-    {
-      dplyr::left_join(
-        tibble::tibble(
-          date = seq.Date(from = ifelse(is.null(begin_date), min((.)$date), begin_date),
-                          to = ifelse(is.null(end_date), max((.)$date), end_date),
-                          by = '1 day')
-        ),
-        (.),
-        by = "date"
-      )
-    } %>%
+    # (OPTIONALLY) return only columns concerning the basic daily SOD (tmin,
+    # tmax, and precip)
     {
       if(select_SOD_cols) {
         (.) %>%
@@ -126,4 +130,5 @@ clean_raw_ghcnd_to_standard <- function(raw_ghcnd_df, select_SOD_cols = TRUE, be
         }
       else (.)
     }
+
 }
